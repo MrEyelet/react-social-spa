7c5898169137540b9cc8ee2a6c931f705a335aee
Marcin <marcin.adam.mroczko@gmail.com>
2020-05-02 21:31:08 +0200

    react router dom

--- app/Main.js
+++ app/Main.js
@@ -1,18 +1,31 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import { BrowserRouter, Switch, Route } from "react-router-dom"
 
 //My components
 import Header from "./components/Header.js"
 import HomeGuest from "./components/HomeGuest.js"
 import Footer from "./components/Footer.js"
+import About from "./components/About.js"
+import Terms from "./components/Terms.js"
 
 function Main() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <HomeGuest />
+      <Switch>
+        <Route path="/" exact>
+          <HomeGuest />
+        </Route>
+        <Route path="/about-us">
+          <About />
+        </Route>
+        <Route path="/terms">
+          <Terms />
+        </Route>
+      </Switch>
       <Footer />
-    </>
+    </BrowserRouter>
   )
 }
 
